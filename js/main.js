c4e07e2f73acaa873829036cb7de6ede34c65fae
Bohdan Skupeiko <115146639+bohdan-skupeiko@users.noreply.github.com>
2023-06-27 18:25:16 +0300

    Day-1

---
+++ js/main.js
@@ -0,0 +1,28 @@
+"use strict";
+a = 5;
+console.log(a);
+
+
+let number= 5;
+const leftBorderWidth = 1;
+
+console.log(number)
+console.log(leftBorderWidth)
+
+
+const obj = {
+	a: 50
+}
+
+obj.a = 90;
+console.log(obj)
+
+
+console.log(name);
+var name = 'Bohdan';
+
+
+{
+	var result = 50;
+}
+console.log(result);
