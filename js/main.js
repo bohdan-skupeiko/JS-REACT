249edad4cedc5206814f8e4b7d7ca1674f4d9aed
Bohdan Skupeiko <115146639+bohdan-skupeiko@users.noreply.github.com>
2023-06-27 19:01:15 +0300

    Less 10-Назви змінних

--- js/main.js
+++ js/main.js
@@ -1,28 +1,26 @@
-"use strict";
-a = 5;
-console.log(a);
+const vehicleBodyWidth = 5000,
+	vehicleBodyLenght = 4000;
+
+console.log("Ширина кузова машини: " + vehicleBodyWidth + ', довниа: ' + vehicleBodyLenght);
+
+// const items = ['asdad', 'asdadsad', 'dasdsa'].map(item =>) {
+// 	console.log(items);
+// }
+
+// //Request
+// date
+// pesponse
 
 
-let number= 5;
-const leftBorderWidth = 1;
+//snake_case
+//UPPER_SNAKE_CASE
+// Kebab-case
+// PascalCase
 
-console.log(number)
-console.log(leftBorderWidth)
+const COLOR_RED = '#FFF';
 
-
-const obj = {
-	a: 50
-}
-
-obj.a = 90;
-console.log(obj)
+const _apiBase = 'https....';
+const _apiKey = 'apikey=d2423eeb....';
 
 
-console.log(name);
-var name = 'Bohdan';
 
-
-{
-	var result = 50;
-}
-console.log(result);
