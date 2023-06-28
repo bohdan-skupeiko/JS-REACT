d5c6cc3726f0d8c76c7c7f5c2e3905bbbf6a845b
Bohdan <bodjaa2101@gmail.com>
2023-06-28 13:13:45 +0300

    first comit

--- js/main.js
+++ js/main.js
@@ -1,26 +1,108 @@
-const vehicleBodyWidth = 5000,
-	vehicleBodyLenght = 4000;
+"use strict"
 
-console.log("Ширина кузова машини: " + vehicleBodyWidth + ', довниа: ' + vehicleBodyLenght);
-
-// const items = ['asdad', 'asdadsad', 'dasdsa'].map(item =>) {
-// 	console.log(items);
+// let number = 4.6;
+//
+// console.log(-4 / 0);
+// console.log("Bohdan" * 9);
+//
+// const person = "Alex";
+// console.log(person);
+//
+// const bool = true;
+// const bloom = false;
+//
+// const obj = {
+// 	name: "Bohdan",
+// 	age: 25,
+// 	isMarried: false
 // }
-
-// //Request
-// date
-// pesponse
-
-
-//snake_case
-//UPPER_SNAKE_CASE
-// Kebab-case
-// PascalCase
-
-const COLOR_RED = '#FFF';
-
-const _apiBase = 'https....';
-const _apiKey = 'apikey=d2423eeb....';
+//
+// console.log(obj);
+// obj.age = 77;
+// console.log(obj["name"]);
+//
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp', 6, obj];
+// console.log(arr[1]);
+// console.log(arr);
 
 
+//=============================DAY-2
 
+// const arr = [1, 2, 3];
+// const arrObj = {
+// 	0: 1,
+// 	1: 2,
+// 	2: 3
+// }
+// console.log(arr[1]);
+// const obj = {
+// 	a: 1,
+// 	b: 2
+// }
+// const objS= {
+// 	'Anna' : 5000,
+// 	'Alice': 800,
+// 	abc: {
+// 		name: 'bohdan',
+// 		age: 26,
+// 		status:{
+// 			df: [],
+// 			isMarried: false,
+// 		}
+// 	}
+// };
+// // objS.b = 12345;
+// // console.log(objS['b']);
+// console.log(objS.abc.status.isMarried);
+// // console.log(objS.b);
+
+
+// const arrBig = ['a', 'b', 'c'];
+// arr[10] = '32423423';
+// console.log(arr[10]);
+// console.log(arr[9]);
+// console.log(arr);
+
+
+// =========================================================
+
+// alert('Hello')
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Are you 18?", "18");
+// console.log(typeof (answer + 5));
+
+// const answers = [];
+//
+//
+// answers[0] = prompt('Ваше імя?', '');
+// answers[1] = prompt('Age', '');
+// answers[2] = prompt('Status', '');
+//
+// console.log(typeof (answers));
+
+
+// const category = 'toys';
+//
+// console.log(`https://someurl.com/${category}/name/4554`);
+//
+//
+// const user = 'Bohdan';
+// alert(`Hello, ${user}`);
+
+// console.log(4 + +" 5");
+//
+// let incr = 10,
+// 	decr = 10;
+//
+// console.log(++incr);
+// console.log(--decr);
+// console.log(5 % 2);
+// console.log(4 + 2 * 2 === 8);
+
+// const isChecked = false,
+// 	isClose = false;
+//
+// console.log(isChecked && isClose);
+// console.log(isChecked || !isClose);
