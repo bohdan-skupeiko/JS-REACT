697f55929077830181861223b24ebb236387be2e
Bohdan <bodjaa2101@gmail.com>
2023-06-28 17:11:23 +0300

    Lesson 21 log operators

--- js/main.js
+++ js/main.js
@@ -1,20 +1,81 @@
 "use strict"
-const numberOfFilms = +prompt("Скільки фільмів ти вже подивився?", '');
 
-const personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false,
-};
-const a = prompt("Один із останніх фільмів які подивився?", ''),
-	b = prompt("На скільки його оцінюєш?", ''),
-	c = prompt("Один із останніх фільмів які подивився?", ''),
-	d = prompt("На скільки його оцінюєш?", '');
+// if (1) {
+// 	console.log('OK!');
+// }else {
+// 	console.log('Error');
+// }
+//
+//
+//
+//
+// if (num < 49) {
+// 	console.log('Error');
+// }else if (num > 100) {
+// 	console.log('Many');
+// }else {
+// 	console.log('Ok!!! ');
+// }
+//
+// (num === 120) ? console.log('Ok!!! ') : console.log('Error');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// const num = '50';
+// switch (num) {
+// 	case '49':
+// 		console.log('Error');
+// 		break;
+// 	case '100':
+// 	console.log('Error');
+// 	break;
+// 	case '51':
+// 		console.log('True');
+// 		break;
+// 	default:
+// 		console.log('Nothing');
+// 		break;
+// }
 
 
-console.log(personalMovieDB);
+//
+// if (hamburger && fries ) {
+// 	console.log("I'am good!!!")
+// }
+//
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 1;
+// console.log(hamburger === 3 && cola === 1 && fries )
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && 'wqqwqqqw');
+
+
+// const hamburger = 5;
+// const fries = 3  ;
+// const cola = 0;
+// const nuggets = 2;
+//
+// if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+// 	console.log("All good!!!");
+// }else {
+// 	console.log("We have problem!!!");
+// }
+// console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
+// console.log(!hamburger);
+
+// let johnReport, alexReport, SamReport, mariaReport = 'done';
+//
+// console.log(johnReport || alexReport || SamReport || mariaReport);
+
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+//
+//
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+// 	console.log('Done!')
+// }
