db34f52a338c87ddf4717357a84f9d494f51c715
Bohdan <bodjaa2101@gmail.com>
2023-06-28 18:49:26 +0300

    Loop in practic

--- js/main.js
+++ js/main.js
@@ -1,81 +1,92 @@
 "use strict"
 
-// if (1) {
-// 	console.log('OK!');
-// }else {
-// 	console.log('Error');
+// let num = 50;
+//
+// while (num <= 55) {
+// 	console.log(num);
+// 	num++;
 // }
-//
-//
-//
-//
-// if (num < 49) {
-// 	console.log('Error');
-// }else if (num > 100) {
-// 	console.log('Many');
-// }else {
-// 	console.log('Ok!!! ');
-// }
-//
-// (num === 120) ? console.log('Ok!!! ') : console.log('Error');
 
-// const num = '50';
-// switch (num) {
-// 	case '49':
-// 		console.log('Error');
+
+// do {
+// 	console.log(num);
+// 	num++;
+// }
+// while (num < 55);
+
+// for (let i = 1; i < 10; i++) {
+// 	if(i === 6) {
+// 		// break;
+// 		continue;
+// 	}
+// 	console.log(i);
+// }
+
+
+// for (let i = 0; i < 3; i++) {
+// 	console.log(i);
+// 	for (let j = 0; j < 3; j++) {
+// 		console.log(j);
+// 	}
+// }
+
+
+// let result = '';
+// const lenght = 10;
+//
+// first: for (let i = 0; i < lenght; i++) {
+// 	console.log(`First lvl: ${i}`)
+// 	for (let j = 0; j < 3; j++) {
+// 		console.log(`Second lvl: ${j}`);
+// 		for (let k = 0; k < 5; k++) {
+// 			if (k == 2) break first;
+// 			console.log(`Third lvl: ${k}`);
+// 		}
+// 	}
+// }
+//
+// console.log(result);
+
+// for (let i = 5; i < 11; i++) {
+// 	console.log(i);
+// }
+
+// for (let i = 20; i > 9; i--) {
+// 	if (i === 13) {
 // 		break;
-// 	case '100':
-// 	console.log('Error');
-// 	break;
-// 	case '51':
-// 		console.log('True');
-// 		break;
-// 	default:
-// 		console.log('Nothing');
-// 		break;
+// 	}
+// 	console.log(i);
+// }
+// for (let i = 2; i < 11; i++) {
+// 	if (i % 2 === 0) {
+// 		console.log(i)
+// 	}
+// }
+
+// let i = 2;
+// while (i <= 16) {
+// 	if (i % 2 !== 0) {
+// 		console.log(i);
+// 	}
+// 	i++;
+// }
+//
+// for (let i = 2; i <= 16; i++) {
+// 	if (i % 2 === 0) {
+// 		continue;
+// 	} else {
+// 		console.log(i);
+// 	}
 // }
 
 
-//
-// if (hamburger && fries ) {
-// 	console.log("I'am good!!!")
-// }
-//
-// const hamburger = 3;
-// const fries = 1;
-// const cola = 1;
-// console.log(hamburger === 3 && cola === 1 && fries )
+function fifthTask() {
+	const arrayOfNumbers = [];
 
-// console.log(1 && 0);
-// console.log(1 && 5);
-// console.log(null && 5);
-// console.log(0 && 'wqqwqqqw');
-
-
-// const hamburger = 5;
-// const fries = 3  ;
-// const cola = 0;
-// const nuggets = 2;
-//
-// if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
-// 	console.log("All good!!!");
-// }else {
-// 	console.log("We have problem!!!");
-// }
-// console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);
-// console.log(!hamburger);
-
-// let johnReport, alexReport, SamReport, mariaReport = 'done';
-//
-// console.log(johnReport || alexReport || SamReport || mariaReport);
-
-
-// const hamburger = 3;
-// const fries = 3;
-// const cola = 0;
-// const nuggets = 2;
-//
-//
-// if (hamburger === 3 && cola || fries === 3 && nuggets) {
-// 	console.log('Done!')
-// }
+	for (let i = 5; i < 11; i++) {
+		arrayOfNumbers.push(i);
+	}
+	// Не трогаем
+	return arrayOfNumbers;
+}
+console.log(fifthTask())
