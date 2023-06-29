f2b346d5e07a4c55e4cc573c2b24d78af2be417c
Bohdan <bodjaa2101@gmail.com>
2023-06-29 17:53:38 +0300

    STR and NUmber

--- js/main.js
+++ js/main.js
@@ -1,37 +1,126 @@
 "use strict"
 
-const numberOfFilms = prompt("Скільки фільмів ви вже подивились?", "");
+// let num = 220;
+//
+// function showFirstMessage(text, age) {
+// 	console.log(text, age);
+// 	num = 21;
+// 	console.log(num);
+// }
+//
+//
+// showFirstMessage('Hello', 25);
+// console.log(num);
+//
+//
+// function calc(a, b) {
+// 	return (a + b);
+// }
+//
+// console.log(calc(2,4));
+//
+//
+// function ret() {
+// 	let num = 50;
+// 	return num;
+// }
+// const anotherNum = ret();
+// console.log(ret());
+// console.log(anotherNum);
+//
+//
+//
+// const  exo = function () {
+// 	console.log("helloo")
+// };
+// exo()
 
-const personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
-}
+//
+// // const calc = (a, b) => a + b;
+// // console.log(calc(3,3));
+// const usdCurr = 28.7;
+// const eurCurr = 30.01;
+// const polCurr = 8.99;
+// const discount = 0.9;
+//
+// function convert (amount, curr) {
+// 	return curr * amount;
+// }
+// convert(400, usdCurr);
+// // convert(400, eurCurr);
+// // convert(400, polCurr);
+//
+// function promotion(result) {
+// 	console.log(result * discount);
+// }
+//
+// const res = convert(400, usdCurr);
+// promotion(res);
+//
+// promotion(convert(400, usdCurr));
 
-for (let i = 0; i < 2; i++) {
-	const a = prompt("Last Film?", ""),
-				b = prompt("Lvl?", "");
-	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-		personalMovieDB.movies[a] = b;
-		console.log("DONE!!!");
-	}else {
-		console.log('Error');
-		i--;
-	}
-}
+// function test() {
+// 	for (let i = 0; i < 5; i++) {
+// 		console.log(i);
+// 		if(i === 3) return
+// 	}
+// 	console.log('Done');
+// }
+//
+// test()
+//
+// // function sayHello(name) {
+// // 	return `$SayHello ${name}`;
+// // }
+// // console.log('Bohdan');
+//
+// function doNothing() {
+// 	return
+// }
+// console.log(doNothing());
+
+// function sayHello(name) {
+// 	return `'Привет, ${name}';`
+// }
+//
+// console.log(sayHello('Bohdan'));
+//
+//
+// function getMathResult(num, repeat) {
+// 	if (typeof(repeat) !== 'number' || repeat <= 0) {
+// 		return num;
+// 	}
+// 	let str = '';
+// 	for (let i = 1; i <= repeat; i++) {
+// 		if (i === repeat) {
+// 			str += `${num * i}`;
+// 		} else {
+// 			str += `${num * i}---`;
+// 		}
+// 	}
+// 	return str;
+// }
+//
+// console.log(4, 5);
 
 
-
-if (personalMovieDB.count < 10 ) {
-	console.log("You see small films");
-}else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-	console.log("You are good man!!!");
-}else if (personalMovieDB.count >= 30) {
-	console.log("You love Movies!!!");
-}else {
-	console.log("Error!!!");
-}
-console.log(personalMovieDB);
-
+// const str = 'test';
+// const arr = [1, 2, 4];
+//
+//
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+//
+// let fruit = 'Some fruit';
+// console.log(fruit.indexOf("q"));
+//
+// const logg = "Hello World";
+// console.log(logg.slice( 3));
+// console.log(logg.substring(3, 5));
+// console.log(logg.substr(2,11));
+//
+// const num = 12.2;
+// console.log(Math.round(num));
+//
+// const test = "12.2px";
+// console.log(parseFloat(test));
